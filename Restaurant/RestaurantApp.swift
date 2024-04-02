7b939a78ef390f685e14a1dd1d7cee56f375d477
naif-alghamdi <naif.gh66@gmail.com>
2024-04-02 22:13:25 +0300

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Naif Alghamdi on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
